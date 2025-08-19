bf8662cfc15fbf3df511b897fd10be47b3da8687
kuro-kano <66070091@kmitl.ac.th>
2025-08-19 15:30:27 +0700

    add: can comment

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 """file app.py to run web server"""
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
+data = []
+
 @app.route("/")
 def main():
-    """main function to render index.html"""
-    return render_template("index.html")
+    """main function what render index.html"""
+    return render_template("index.html", data=data)
+
+@app.route("/add", methods=["POST"])
+def add_comment():
+    """add comment to data[]"""
+    yourname = request.form.get("yourname")
+    message = request.form.get("message")
+
+    if yourname and message:
+        data.append({"yourname": yourname, "message": message})
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, threaded=False)
