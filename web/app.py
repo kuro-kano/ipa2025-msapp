8793f94224cc9919df07e7f3201de946afcfea11
kuro-kano <66070091@kmitl.ac.th>
2025-08-19 16:32:17 +0700

    add: docker-compose.yml modified: changed all data what collect on local to collect data on  mongodb changed: directory structure to use docker-compose.yml

--- web/app.py
+++ web/app.py
@@ -1,23 +1,37 @@
 """file app.py to run web server"""
 from flask import Flask, request, render_template, redirect, url_for
+from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
+client = MongoClient("mongodb://mongo:27017/")
+db = client['ipa2025_db']
+routers = db["routers"]
 
 data = []
 
 @app.route("/")
 def main():
     """main function what render index.html"""
-    return render_template("index.html", data=data)
+    for x in routers.find():
+        data.append(x)
+    return render_template("index.html", routers=data)
 
 @app.route("/add", methods=["POST"])
 def add_router():
     """add router to data[]"""
     ip = request.form.get("ip")
     username = request.form.get("username")
+    password = request.form.get("password")
 
-    if ip and username:
-        data.append({"ip": ip, "username": username})
+    if ip and username and password:
+        routers.insert_one(
+            {
+                "ip": ip,
+                "username": username,
+                "password": password
+            }
+        )
     return redirect("/")
 
 @app.route("/delete/<idx>", methods=["POST"])
@@ -26,8 +40,7 @@ def delete_router(idx):
     idx = int(idx)
 
     try:
-        if 0 <= idx < len(data):
-            data.pop(idx)
+        routers.delete_one({ "_id": ObjectId(idx) })
     except Exception:
         pass
 
