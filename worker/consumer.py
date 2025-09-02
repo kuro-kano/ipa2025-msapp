b6fc8ba2aebb47dd58c8dcd261b73e16a31190a7
kuro-kano <66070091@kmitl.ac.th>
2025-09-02 12:53:19 +0700

    add: worker what can consume jobs from RabbittMQ already!

---
+++ worker/consumer.py
@@ -0,0 +1,40 @@
+""""""
+import os, time, pika
+
+username = os.getenv('RABBITMQ_USERNAME')
+password = os.getenv('RABBITMQ_PASSWORD')
+
+def callback(ch, method, props, body):
+    """Callback function to process received messages from RabbitMQ."""
+    print(f"body: {body.decode()}")
+    time.sleep(3)
+
+def consume(host):
+    """"""
+    for attempt in range(10):
+        try:
+            print(f"Connecting tot RabbitMQ (try {attempt})...")
+
+            credentials = pika.PlainCredentials(username, password)
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(
+                    host=host,
+                    credentials=credentials
+                )
+            )
+            break
+
+        except Exception as e:
+            print(f"Connection failed: {e}")
+            time.sleep(5)
+    else:
+        print("Could not connect after 10 attempts, exiting..."); exit(1)
+
+    channel = connection.channel()
+    channel.queue_declare(queue="router_jobs")
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue="router_jobs", on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
+
+if __name__ == "__main__":
+    consume("localhost")
