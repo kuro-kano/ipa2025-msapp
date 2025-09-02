ed322bdcee897c3bea97b2e9267849603af0ed82
kuro-kano <66070091@kmitl.ac.th>
2025-09-03 01:05:48 +0700

    fixed: flake8 (pep8) round 3

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -27,7 +27,11 @@ def produce(host, body):
     channel.queue_bind(
         queue="router_jobs", exchange="jobs", routing_key="check_interfaces"
     )
-    channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
+    channel.basic_publish(
+        exchange="jobs",
+        routing_key="check_interfaces",
+        body=body
+    )
 
     connection.close()
 
